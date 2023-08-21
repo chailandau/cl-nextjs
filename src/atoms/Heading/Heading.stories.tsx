f2cb4e6f1d571e4c335a62bc9cf3b734149a74fa
Chai Landau <chai.landau@gmail.com>
2023-08-21 17:54:57 -0400

    test: set up jest and react testing library * docs: set up storybook * chore: update package.json storybook scripts * feat: add Heading atom

---
+++ src/atoms/Heading/Heading.stories.tsx
@@ -0,0 +1,71 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Heading, { headingColors } from './Heading';
+
+const meta: Meta<typeof Heading> = {
+    title: 'Atoms/Heading',
+    component: Heading,
+    argTypes: {
+        size: {
+            control: 'select',
+            table: { defaultValue: { summary: 'md' } }
+        },
+        as: {
+            table: { defaultValue: { summary: 'h2' } }
+        },
+        color: {
+            control: 'select',
+            options: headingColors,
+            table: { defaultValue: { summary: 'green' } }
+        }
+    }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Heading>;
+
+export const Default: Story = {
+    args: {
+        as: 'h2',
+        size: 'md',
+        children: 'Default heading',
+        color: 'green'
+    }
+};
+
+export const ExtraSmall: Story = {
+    args: {
+        as: 'h2',
+        size: 'xs',
+        children: 'Extra small heading',
+        color: 'green'
+    }
+};
+
+export const Small: Story = {
+    args: {
+        as: 'h2',
+        size: 'sm',
+        children: 'Small heading',
+        color: 'green'
+    }
+};
+
+export const Large: Story = {
+    args: {
+        as: 'h2',
+        size: 'lg',
+        children: 'Large heading',
+        color: 'green'
+    }
+};
+
+export const ExtraLarge: Story = {
+    args: {
+        as: 'h2',
+        size: 'xl',
+        children: 'Extra large heading',
+        color: 'green'
+    }
+};
---
+++ src/atoms/Heading/Heading.test.tsx
@@ -0,0 +1,64 @@
+import { composeStory } from '@storybook/react';
+import { render, screen } from '@testing-library/react';
+
+import { headingColors, headingSizes, headingTags } from './Heading';
+import Meta, {
+    Default,
+    ExtraLarge,
+    ExtraSmall,
+    Large,
+    Small
+} from './Heading.stories';
+
+import {
+    testAxeAndSnapshot,
+    testPropOptions,
+    testTags
+} from '@/utils/testHelpers';
+
+const getHeadingInfo = (Variant: typeof Default) => ({
+    variant: composeStory(Variant, Meta),
+    text: Variant.args?.children || '',
+    color: Variant.args?.color || ''
+});
+
+const headingVariants = [
+    getHeadingInfo(Default),
+    getHeadingInfo(ExtraSmall),
+    getHeadingInfo(Small),
+    getHeadingInfo(Large),
+    getHeadingInfo(ExtraLarge)
+];
+
+describe('Heading', () => {
+    const Heading = composeStory(Default, Meta);
+    testPropOptions({
+        component: <Heading />,
+        propName: 'size',
+        propOptions: headingSizes,
+        htmlTag: 'h2'
+    });
+});
+
+headingVariants.forEach(({ variant: Heading, text }) => {
+    const headingComponent = {
+        component: <Heading />
+    };
+    describe(text, () => {
+        it('has defaults', () => {
+            render(
+                <Heading as={undefined} color={undefined} size={undefined} />
+            );
+            expect(screen.getByRole('heading')).toHaveClass('black', 'md');
+            expect(screen.getByRole('heading', { level: 2 })).toBeDefined;
+        });
+        testTags({ ...headingComponent, tags: headingTags });
+        testPropOptions({
+            ...headingComponent,
+            propName: 'color',
+            propOptions: headingColors,
+            htmlTag: 'h2'
+        });
+        testAxeAndSnapshot(headingComponent);
+    });
+});
