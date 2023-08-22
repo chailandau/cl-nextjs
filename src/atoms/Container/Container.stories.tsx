78010da7d4d7917e69f079fa2724afe101f74b5c
Chai Landau <chai.landau@gmail.com>
2023-08-22 15:20:29 -0400

    feat: add container atom

---
+++ src/atoms/Container/Container.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import Container from './Container';
+
+import Text from '@/atoms/Text/Text';
+
+const textAtom = <Text>This text is in a container.</Text>;
+
+const meta: Meta<typeof Container> = {
+    title: 'Atoms/Container',
+    component: Container,
+    argTypes: {
+        as: {
+            table: { defaultValue: { summary: 'div' } }
+        }
+    },
+    parameters: {
+        controls: {
+            include: ['as', 'className']
+        }
+    }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Container>;
+
+export const Default: Story = {
+    args: {
+        children: textAtom
+    }
+};
