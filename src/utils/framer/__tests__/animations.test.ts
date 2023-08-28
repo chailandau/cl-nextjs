a807b2928a58956c6f0a1533b11e94168fb60ed0
Chai Landau <chai.landau@gmail.com>
2023-08-28 12:16:47 -0400

    refactor: remove dropdownAnimations

--- src/utils/framer/__tests__/animations.test.ts
+++ src/utils/framer/__tests__/animations.test.ts
@@ -1,18 +1,5 @@
 import * as framerAnimations from '../animations';
-import { dropdownAnimations } from '../animations';
 
-describe('dropdownAnimations', () => {
-    it('returns correct animation when laptop is true', () => {
-        const isLaptop = true;
-        const result = dropdownAnimations(isLaptop);
-        expect(result).toMatchSnapshot();
-    });
-    it('returns correct animation when laptop is false', () => {
-        const isLaptop = false;
-        const result = dropdownAnimations(isLaptop);
-        expect(result).toMatchSnapshot();
-    });
-});
 describe('Framer animations', () => {
     Object.keys(framerAnimations).forEach((animationName) => {
         const animation =
--- src/utils/framer/animations.ts
+++ src/utils/framer/animations.ts
@@ -18,26 +18,6 @@ export const menuAnimations = {
     }
 };
 
-export const dropdownAnimations = (isLaptop: boolean | null) => ({
-    closed: {
-        y: '15%',
-        x: isLaptop ? 'calc(-50% - 6px)' : 0,
-        opacity: 0,
-        transition: {
-            duration: 0.25
-        }
-    },
-    open: {
-        y: 0,
-        x: isLaptop ? 'calc(-50% - 6px)' : 0,
-        opacity: 1,
-        transition: {
-            type: 'spring',
-            duration: 0.25
-        }
-    }
-});
-
 export const iconFlip = {
     closed: { rotate: 0, transition: { type: 'spring', duration: 0.25 } },
     open: {
