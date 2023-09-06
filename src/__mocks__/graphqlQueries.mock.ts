614f32840b9d0349ee489998fa4cf28a296cdc39
Chai Landau <chai.landau@gmail.com>
2023-09-05 20:23:55 -0400

    chore: update loki

--- src/__mocks__/graphqlQueries.mock.ts
+++ src/__mocks__/graphqlQueries.mock.ts
@@ -6,5 +6,32 @@ export const mockDefaults = {
         width: () => 64,
         height: () => 64
     }),
-    Cta_linkType: () => 'internal'
+    Cta_linkType: () => 'internal',
+    Page_Sections: () => ({
+        __typename: 'HeroBlock',
+        hero: {
+            coloredSubhead: null,
+            cta: {
+                externalLink: 'Hello World',
+                id: 'Hello World',
+                internalLink: {
+                    id: 'Hello World',
+                    slug: 'Hello World'
+                },
+                label: 'Hello World',
+                linkType: 'internal'
+            },
+            heading: 'Hello World',
+            headingTag: 'Hello World',
+            icon: {
+                alt: 'Hello World',
+                height: 64,
+                id: 'Hello World',
+                url: 'Hello World',
+                width: 64
+            },
+            id: 'Hello World'
+        },
+        id: 'Hello World'
+    })
 };
